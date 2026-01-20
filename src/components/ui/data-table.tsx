75913d1f343dd0353b751d2372d09d93662209c3
AlessandroSampaio <alessandrosampaio521@gmail.com>
2026-01-20 11:39:24 -0300

    feat: adds table and data-table components

---
+++ src/components/ui/data-table.tsx
@@ -0,0 +1,144 @@
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  createSolidTable,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+} from "@tanstack/solid-table";
+import { createSignal, For, Show } from "solid-js";
+import { Button } from "~/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>(props: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = createSignal<SortingState>([]);
+  const [rowSelection, setRowSelection] = createSignal({});
+  const [columnFilters, setColumnFilters] = createSignal<ColumnFiltersState>(
+    [],
+  );
+
+  const table = createSolidTable({
+    get data() {
+      return props.data;
+    },
+    get columns() {
+      return props.columns;
+    },
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    onSortingChange: setSorting,
+    onRowSelectionChange: setRowSelection,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      get sorting() {
+        return sorting();
+      },
+      get columnFilters() {
+        return columnFilters();
+      },
+      get rowSelection() {
+        return rowSelection();
+      },
+    },
+  });
+
+  return (
+    <div class="flex-1">
+      <div class="rounded-md border h-full">
+        <Table>
+          <TableHeader>
+            <For each={table.getHeaderGroups()}>
+              {(headerGroup) => (
+                <TableRow>
+                  <For each={headerGroup.headers}>
+                    {(header) => (
+                      <TableHead colSpan={header.colSpan}>
+                        <Show when={!header.isPlaceholder}>
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                        </Show>
+                      </TableHead>
+                    )}
+                  </For>
+                </TableRow>
+              )}
+            </For>
+          </TableHeader>
+          <TableBody>
+            <Show
+              when={table.getRowModel().rows?.length}
+              fallback={
+                <TableRow>
+                  <TableCell
+                    colSpan={props.columns.length}
+                    class="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              }
+            >
+              <For each={table.getRowModel().rows}>
+                {(row) => (
+                  <TableRow data-state={row.getIsSelected() && "selected"}>
+                    <For each={row.getVisibleCells()}>
+                      {(cell) => (
+                        <TableCell>
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext(),
+                          )}
+                        </TableCell>
+                      )}
+                    </For>
+                  </TableRow>
+                )}
+              </For>
+            </Show>
+          </TableBody>
+        </Table>
+      </div>
+      <div class="flex justify-between">
+        <div class="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} de{" "}
+          {table.getFilteredRowModel().rows.length} registros selecionados.
+        </div>
+        <div class="flex items-center justify-end space-x-2 py-4">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
