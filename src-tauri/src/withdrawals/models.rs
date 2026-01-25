948699f248fe11779edaa275343ea0b15f072221
AlessandroSampaio <alessandrosampaio521@gmail.com>
2026-01-25 16:13:17 -0300

    feat: implt WithdrawalService create_withdrawal trait fn

--- src-tauri/src/withdrawals/models.rs
+++ src-tauri/src/withdrawals/models.rs
@@ -12,3 +12,13 @@ pub struct Withdrawal {
     pub quantity: BigDecimal,
     pub date: chrono::NaiveDate,
 }
+
+#[taurpc::ipc_type]
+#[derive(Insertable, AsChangeset, Debug)]
+#[diesel(table_name = withdrawals)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct NewWithdrawal {
+    pub tank_id: i32,
+    pub quantity: BigDecimal,
+    pub date: chrono::NaiveDate,
+}
--- src-tauri/src/withdrawals/service.rs
+++ src-tauri/src/withdrawals/service.rs
@@ -1,9 +1,14 @@
-use crate::errors::IpcError;
-use crate::withdrawals::models::Withdrawal;
+use crate::{
+    db::establish_connection,
+    errors::IpcError,
+    schema::withdrawals::dsl::*,
+    withdrawals::models::{NewWithdrawal, Withdrawal},
+};
+use diesel::{prelude::*, SelectableHelper};
 
 #[taurpc::procedures(path = "withdrawals")]
 pub trait WithdrawalsService {
-    async fn create_withdrawal(new_withdrawal: Withdrawal) -> Result<Withdrawal, IpcError>;
+    async fn create_withdrawal(new_withdrawal: NewWithdrawal) -> Result<Withdrawal, IpcError>;
 }
 
 #[derive(Debug, Clone)]
@@ -11,9 +16,19 @@ pub struct WithdrawalsServiceImpl;
 
 #[taurpc::resolvers]
 impl WithdrawalsService for WithdrawalsServiceImpl {
-    async fn create_withdrawal(self, new_withdrawal: Withdrawal) -> Result<Withdrawal, IpcError> {
-        // Implementation goes here
+    async fn create_withdrawal(
+        self,
+        new_withdrawal: NewWithdrawal,
+    ) -> Result<Withdrawal, IpcError> {
         println!("Creating withdrawal: {:?}", new_withdrawal);
-        unimplemented!()
+
+        let connection = &mut establish_connection();
+
+        let created_withdrawal = diesel::insert_into(withdrawals)
+            .values(new_withdrawal)
+            .returning(Withdrawal::as_returning())
+            .get_result(connection)?;
+
+        Ok(created_withdrawal)
     }
 }
