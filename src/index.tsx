6f428e4cf46d1590dc5f040e99e0346942e8a59c
AlessandroSampaio <alessandrosampaio521@gmail.com>
2026-02-05 16:43:41 -0300

    feat: adds a splash screen to cover while running pending migrations

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,17 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 import App from "./App";
+import { onMount } from "solid-js";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+const AppWrappper = () => {
+  onMount(async () => {
+    const appWindow = getCurrentWindow();
+
+    await appWindow.show();
+  });
+
+  return <App />;
+};
+
+render(() => <AppWrappper />, document.getElementById("root") as HTMLElement);
--- src/pages/splash-screen.tsx
+++ src/pages/splash-screen.tsx
@@ -1,11 +1,24 @@
 import { A } from "@solidjs/router";
+import { createSignal, onMount } from "solid-js";
 import { Button } from "~/components/ui/button";
+import { createTauRPCProxy } from "~/types/rpc";
 
 export const SplashScreen = () => {
+  const rpc = createTauRPCProxy();
+
+  const [msg, setMsg] = createSignal("");
+
+  onMount(() =>
+    rpc.setup.on_progress((progress) => {
+      console.log(`Progress (${progress.progress}%): ${progress.message}`);
+      setMsg(`Progress (${progress.progress}%): ${progress.message}`);
+    }),
+  );
   return (
     <div class="size-full flex flex-col items-center justify-center gap-4">
       <h1 class="text-3xl">Splash Screen</h1>
       <h2 class="text-xl">Say hi to me!!</h2>
+      <span>{msg()}</span>
       <Button as={A} href="/home">
         Go Home
       </Button>
